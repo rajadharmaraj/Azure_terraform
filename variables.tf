43be48e4a1783ba89595713beb523ac198c89f1d
rajadharmaraj <10411141+rajadharmaraj@users.noreply.github.com>
2025-01-09 23:23:14 -0500

    Update variables.tf

---
+++ variables.tf
@@ -0,0 +1,38 @@
+variable "environment" {
+  description = "Environment name (dev, staging, prod)"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region where resources will be created"
+  type        = string
+  default     = "eastus"
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+}
+
+variable "storage_account_name" {
+  description = "Name of the storage account"
+  type        = string
+}
+
+variable "storage_account_tier" {
+  description = "Storage account tier (Standard or Premium)"
+  type        = string
+  default     = "Standard"
+}
+
+variable "storage_account_replication" {
+  description = "Storage account replication type"
+  type        = string
+  default     = "LRS"
+}
+
+variable "tags" {
+  description = "Tags to apply to all resources"
+  type        = map(string)
+  default     = {}
+}
