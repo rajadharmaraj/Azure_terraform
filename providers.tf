28f281f84984ae1e51b77152cdffc245cf24a5f5
rajadharmaraj <raja_pravin@yahoo.com>
2025-01-11 13:19:47 -0500

    commit the provider.tf

--- providers.tf
+++ providers.tf
@@ -5,6 +5,17 @@ terraform {
       version = "~> 3.0"
     }
   }
+  terraform {
+  required_providers {
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.15.0"
+    }
+  }
+# Configure the Azure Active Directory Provider
+provider "azuread" {
+  tenant_id = "00000000-0000-0000-0000-000000000000"
+}
   backend "azurerm" {
     # Configure your backend settings here
   }
